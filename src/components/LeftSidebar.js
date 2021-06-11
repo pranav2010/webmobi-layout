5ccb9820bb436694bfb2ae13572ae34fc60c832e
pranav2010 <63247413+pranav2010@users.noreply.github.com>
2021-06-11 12:12:09 +0530

    intialised all files

---
+++ src/components/LeftSidebar.js
@@ -0,0 +1,18 @@
+import React from "react";
+import {
+  BsFillBellFill,
+  IoIosPeople,
+  IoMdAnalytics,
+  GoMailRead,
+} from "react-icons/bs";
+
+function Sidebar() {
+  return (
+    <div className="side-bar">
+      <BsFillBellFill style={{ color: "blue" }} />
+      sidebar
+    </div>
+  );
+}
+
+export default Sidebar;
---
+++ src/pages/Teams.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Teams.css";
+import Sidebar from "../components/LeftSidebar";
+
+function Teams() {
+  return (
+    <div className="container-fluid">
+      <Sidebar />
+
+      <div className="topbar"></div>
+      <div className=""></div>
+    </div>
+  );
+}
+
+export default Teams;
