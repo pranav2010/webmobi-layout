b57fb9da85b09669471a307a7558dadb0d98896d
pranav2010 <63247413+pranav2010@users.noreply.github.com>
2021-06-13 17:13:51 +0530

    Stylised most components

--- src/components/CenterBox.js
+++ src/components/CenterBox.js
@@ -1,45 +1,124 @@
-import React from "react";
-import { IoMdChatboxes, IoIosCloseCircleOutline } from "react-icons/io";
+import React, { useState } from "react";
+import {
+  IoMdChatboxes,
+  IoIosCloseCircleOutline,
+  IoMdLock,
+} from "react-icons/io";
 import details from "./settingdetails";
 
 function CenterBox() {
+  // function for individual settings
   function handleDetails(content) {
     return (
       <div className="individual-settings">
         <p style={{ fontSize: "16px" }}>{content.maintext}</p>
-        <p style={{ fontSize: "12px" }}>{content.subtext}</p>
+        <p style={{ fontSize: "12px", color: "#6b6b6b" }}>{content.subtext}</p>
       </div>
     );
   }
 
+  /// function for click
+  const [settingClick, setSettingClick] = useState(3);
+
   return (
     <div className="center-box">
+      {/* Setting names in left of center box  */}
       <div className="setting-name">
-        <p style={{ color: "#e1e8eb" }}>Event Settings</p>
-        <hr style={{ color: "gray", size: "100" }}></hr>
-        <p style={{ fontWeight: "normal", padding: "6px 6px" }}>General</p>
-        <p style={{ fontWeight: "normal", padding: "6px 6px" }}>Privacy</p>
-        <p style={{ fontWeight: "normal", padding: "6px 6px" }}>Features</p>
-        <p style={{ fontWeight: "normal", padding: "6px 6px" }}>
+        <h5
+          style={{
+            color: "#727272",
+            fontWeight: "normal",
+            fontSize: "19px",
+            padding: "6px 15px",
+            margin: ".2rem 0",
+          }}
+        >
+          Event Settings
+        </h5>
+        <hr style={{ color: "gray", size: "100", margin: "8px 8px 19px" }}></hr>
+        <p
+          onClick={() => {
+            setSettingClick(1);
+          }}
+          className={settingClick === 1 ? "p-after" : "p-before"}
+        >
+          General
+        </p>
+        <p
+          onClick={() => {
+            setSettingClick(2);
+          }}
+          className={settingClick === 2 ? "p-after" : "p-before"}
+        >
+          Privacy
+        </p>
+        <p
+          onClick={() => {
+            setSettingClick(3);
+          }}
+          className={settingClick === 3 ? "p-after" : "p-before"}
+        >
+          Features
+        </p>
+        <p
+          onClick={() => {
+            setSettingClick(4);
+          }}
+          className={settingClick === 4 ? "p-after" : "p-before"}
+        >
           Customization
         </p>
-        <p style={{ fontWeight: "normal", padding: "6px 6px" }}>Integration</p>
-        <p style={{ fontWeight: "normal", padding: "6px 6px" }}>
-          Session Settings
+        <p
+          onClick={() => {
+            setSettingClick(5);
+          }}
+          className={settingClick === 5 ? "p-after" : "p-before"}
+        >
+          Integration
         </p>
-        <p style={{ fontWeight: "normal", padding: "6px 6px" }}>My plans</p>
+        <p
+          onClick={() => {
+            setSettingClick(6);
+          }}
+          className={settingClick === 6 ? "p-after" : "p-before"}
+        >
+          Session Settings{" "}
+          <IoMdLock style={{ color: "#6b6b6b", paddingBottom: "2px" }} />
+        </p>
+        <p
+          onClick={() => {
+            setSettingClick(7);
+          }}
+          className={settingClick === 7 ? "p-after" : "p-before"}
+        >
+          My plans
+        </p>
       </div>
 
+      {/* right side of center box */}
       <div className="setting-content">
         <div className="settings-heading my-4">
-          <div style={{ padding: "10px 30px 0px 30px" }}>
+          <div style={{ padding: "10px 30px 0px 30px", color: "#8f00ff" }}>
             <IoMdChatboxes size="30px" />
             <span style={{ marginLeft: "6px", fontWeight: "bolder" }}>
               Audience Q&A
             </span>
+            {/* <div class="form-check form-switch">
+              <input
+                class="form-check-input"
+                type="checkbox"
+                id="flexSwitchCheckDefault"
+              />
+            </div> */}
+
             <hr style={{ width: "70%", margin: "8px" }}></hr>
           </div>
           {details.map(handleDetails)}
+          <div className="save-btn">
+            <button type="button" class="btn btn-primary">
+              Save
+            </button>
+          </div>
         </div>
 
         <div className="close-icon">
--- src/components/LeftSidebar.js
+++ src/components/LeftSidebar.js
@@ -1,25 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 import { BsFillBellFill } from "react-icons/bs";
 import { IoIosPeople, IoMdAnalytics } from "react-icons/io";
 import { GoMailRead } from "react-icons/go";
+import { RiBarChartBoxFill } from "react-icons/ri";
 
 function LeftSidebar() {
+  const [iconClick, setIconclick] = useState(1);
+
+  function handleClick() {
+    setIconclick();
+  }
+
   return (
     <div className="left-side-bar">
-      <div className="left-icons mx-3">
-        <BsFillBellFill size="28px" />
+      <div className="webmobi-logo"></div>
+      <div
+        onClick={() => {
+          setIconclick(1);
+        }}
+        className={
+          iconClick === 1 ? "left-icons-clicked mx-3" : "left-icons mx-3"
+        }
+      >
+        <RiBarChartBoxFill size="28px" />
         <p>Events</p>
       </div>
 
-      <div className="left-icons mx-3">
+      <div
+        onClick={() => {
+          setIconclick(2);
+        }}
+        className={
+          iconClick === 2 ? "left-icons-clicked mx-3" : "left-icons mx-3"
+        }
+      >
         <IoIosPeople size="28px" />
         <p>Teams</p>
       </div>
-      <div className="left-icons mx-3">
+      <div
+        onClick={() => {
+          setIconclick(3);
+        }}
+        className={
+          iconClick === 3 ? "left-icons-clicked mx-3" : "left-icons mx-3"
+        }
+      >
         <IoMdAnalytics size="28px" />
         <p>Analytics</p>
       </div>
-      <div className="left-icons mx-3">
+      <div
+        onClick={() => {
+          setIconclick(4);
+        }}
+        className={
+          iconClick === 4 ? "left-icons-clicked mx-3" : "left-icons mx-3"
+        }
+      >
         <GoMailRead size="28px" />
         <p>Results</p>
       </div>
